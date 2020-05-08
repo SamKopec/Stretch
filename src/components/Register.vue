05215e8441e05ca630e43c021b3057c90bd64652
sam kopec <skopec25@gmail.com>
2020-05-08 12:32:46 -0700

    create login and register components with correct routing

---
+++ src/components/Register.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="login-container">
+		<div class="form-container">
+			<div class="login-label">
+				<h3 class="red-text small-text">Sign Up</h3>
+			</div>
+			<div>
+				<h3 class="blue-text tiny-text">First Name</h3>
+				<input class="login-input full-input blue-text tiny-text" type="text" />
+			</div>
+			<div>
+				<h3 class="blue-text tiny-text">Email</h3>
+				<input class="login-input full-input blue-text tiny-text" type="text" />
+			</div>
+			<div>
+				<h3 class="blue-text tiny-text">Password</h3>
+				<input class="login-input full-input blue-text tiny-text" type="text" />
+			</div>
+			<div class="login-label red-text hover-red small-text">Sign up</div>
+			<div class="login-label blue-text tiny-text">
+				Existing Account?
+				<router-link :to="{ path: '/login' }">
+					<span class="red-text hover-red sign-up-margin">Login</span>
+				</router-link>
+			</div>
+		</div>
+		<app-circle></app-circle>
+	</div>
+</template>
+
+<script>
+import Circle from "./Circle.vue";
+export default {
+	data() {
+		return {};
+	},
+	components: {
+		"app-circle": Circle
+	}
+};
+</script>
+
+<style scoped>
+.full-input {
+	width: 100%;
+}
+
+.login-input {
+	border-bottom: 3px solid #2ab7ca;
+}
+
+.login-label {
+	display: flex;
+	justify-content: center;
+	margin-bottom: 10px;
+	margin-top: 20px;
+}
+
+.login-container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+}
+.form-container {
+	/*background-color: turquoise;*/
+	color: white;
+	width: 50%;
+	height: 85%;
+}
+
+.sign-up-margin {
+	margin-left: 10px;
+}
+
+@media screen and (min-width: 850px) {
+	.full-input {
+		width: 80%;
+	}
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ import Dashboard from "./components/Dashboard.vue";
 import Timer from "./components/Timer.vue";
 import NewSession from "./components/NewSession.vue";
 import EditSession from "./components/EditSession.vue";
+import Login from "./components/Login.vue";
+import Register from "./components/Register.vue";
 
 export const routes = [
 	{ path: "", component: LandingPage },
 	{ path: "/dashboard", name: "dashboard", component: Dashboard },
 	{ path: "/stretch/:id", component: Timer, name: "timer", props: true },
 	{ path: "/newsession", component: NewSession, props: true },
+	{ path: "/login", component: Login },
+	{ path: "/new-user", component: Register },
 	{
 		path: "/editsession/:id",
 		component: EditSession,
