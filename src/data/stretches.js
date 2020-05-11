f0fdced4efd38ec3c80e5ce312e33a9f1e9e934c
sam kopec <skopec25@gmail.com>
2020-05-11 16:34:22 -0700

    moves available stretches to data folder

---
+++ src/data/stretches.js
@@ -0,0 +1,63 @@
+export const stretchesList = [
+	{
+		name: "Forward Stretch",
+		icon: "ForwardFull.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{
+		name: "Forward Bend",
+		icon: "ForwardBendFull.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{
+		name: "Butterfly",
+		icon: "ButterflyFull.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{
+		name: "Leg Raise",
+		icon: "LegRaiseFull.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{ name: "Lunge", icon: "LungeFull.svg", minutes: "1", seconds: "0" },
+	{
+		name: "Single Leg Forward",
+		icon: "SingleLegForwardFull.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{
+		name: "Step Forward",
+		icon: "StepForwardFull.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{
+		name: "Calf Stretch",
+		icon: "CalfStretchFull.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{
+		name: "Quad Stretch",
+		icon: "QuadStretchFull.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{
+		name: "Overhead Stretch",
+		icon: "OverHeadStretch.svg",
+		minutes: "1",
+		seconds: "0"
+	},
+	{
+		name: "Cross Arm Stretch",
+		icon: "CrossArmFull.svg",
+		minutes: "1",
+		seconds: "0"
+	}
+];
